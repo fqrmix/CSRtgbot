7b7f56f06a5efb82cc60d01476a2ba5f8604b932
fqrmix <semen.cavallini@gmail.com>
2022-05-09 22:00:48 +0300

    Split files and minor changes

---
+++ csrlib.py
@@ -0,0 +1,36 @@
+import pathlib
+
+# Auth
+token = 'mytoken'
+
+# Some functions
+
+
+def convert(data):
+    if isinstance(data, bytes):
+        return data.decode('ascii')
+
+    if isinstance(data, dict):
+        return dict(map(convert, data.items()))
+
+    if isinstance(data, tuple):
+        return map(convert, data)
+
+    return data
+
+
+def csrCheck(csr_file, file_info):
+    if file_info.file_path.endswith('.csr'):
+        print('.csr found!')
+        strFile = convert(csr_file).splitlines()
+        if strFile[0] == "-----BEGIN CERTIFICATE REQUEST-----":
+            print('start is OK')
+            if strFile[len(strFile) - 1] == "-----END CERTIFICATE REQUEST-----":
+                print('end is OK')
+                return True
+            else:
+                raise Exception(f'Окончание запроса на сертификат некорректно!\n{strFile[len(strFile) - 1]}')
+        else:
+            raise Exception(f'Начало запроса на сертификат некорректно!\n{strFile[0]}')
+    else:
+        raise Exception(f'У файла, который ты прислал, расширение не .CSR, а "{pathlib.Path(file_info.file_path).suffix}"')
--- main.py
+++ main.py
@@ -1,51 +1,19 @@
 import telebot
+from csrlib import token, convert, csrCheck
 import OpenSSL.crypto
 from OpenSSL.crypto import load_certificate_request, FILETYPE_PEM
 
-token = 'mytoken'
-
 bot = telebot.TeleBot(token)
+botWelcomeMessage = 'Привет!\nДля декодинга .CSR файла просто пришли его мне!'
 
 
-def convert(data):
-    if isinstance(data, bytes):
-        return data.decode('ascii')
-
-    if isinstance(data, dict):
-        return dict(map(convert, data.items()))
-
-    if isinstance(data, tuple):
-        return map(convert, data)
-
-    return data
-
-
-def csrCheck(csr_file, file_info):
-    if file_info.file_path.endswith('.csr'):
-        print('.csr found!')
-        strFile = convert(csr_file).splitlines()
-        if strFile[0] == "-----BEGIN CERTIFICATE REQUEST-----":
-            print('start is OK')
-            if strFile[len(strFile) - 1] == "-----END CERTIFICATE REQUEST-----":
-                print('end is OK')
-                return True
-            else:
-                print('end is not OK!')
-                return False
-        else:
-            print('start is not OK')
-    else:
-        print('Invalid extension of file!')
-        return False
-
-
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, botWelcomeMessage)
 
 
 @bot.message_handler(content_types=['document'])
-def handle_docs_photo(message):
+def send_csr_decode(message):
     try:
         chat_id = message.chat.id
         file_info = bot.get_file(message.document.file_id)
@@ -65,9 +33,7 @@ def handle_docs_photo(message):
                                   f"Country: {str_components ['C']}\n"
                                   f"Key algorithm: {key_type}\n"
                                   f"Key size: {key.bits()}")
-        else:
-            raise Exception('Invalid CSR!')
-
+           
     except Exception as e:
         bot.reply_to(message, e)
 
